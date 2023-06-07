78c62a0e4e9de082dd10f83796bcc708172809df
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-06-07 10:27:42 -0700

    Brought matrixCalculations to its own lib folder

--- src/components/MatriXpert.jsx
+++ src/components/MatriXpert.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "../index.css";
 import Header from "./Header/Header.jsx";
 import Hero from "./Hero/Hero.jsx";
+import MatrixCalculations from "../lib/matrixCalculations";
 
 export default function MatriXpert() {
+  const test = new MatrixCalculations();
+  console.log(test.addMatrices([[2, 2], [1, 4]], [[1, 4],[2, 2]]))
   return (
     <>
       <Header></Header>
