5f67dacdd71e1254023e7a9778e3fbd05200007e
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-06-08 11:47:59 -0700

    Fixed indent

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,97 +6,97 @@ import MatrixResult from "./MatrixResult/MatrixResult";
 import Navbar from "../Navbar/Navbar";
 
 export const OPERATION_LIST = [
-	{
-		id: "add",
-		title: "Addition",
-		requiresTwoMatrices: true,
-		requiresConstant: false,
-	},
-	{
-		id: "subtract",
-		title: "Subtraction",
-		requiresTwoMatrices: true,
-		requiresConstant: false,
-	},
-	{
-		id: "multiplication",
-		title: "Multiplication",
-		requiresTwoMatrices: true,
-		requiresConstant: false,
-	},
-	{
-		id: "k-multiplication",
-		title: "K-Multiplication",
-		requiresTwoMatrices: false,
-		requiresConstant: true,
-	},
-	{
-		id: "transpose",
-		title: "Transpose",
-		requiresTwoMatrices: false,
-		requiresConstant: false,
-	},
-	{
-		id: "determinant",
-		title: "Determinant",
-		requiresTwoMatrices: false,
-		requiresConstant: false,
-	},
-	{
-		id: "cramers",
-		title: "Cramer's Rule",
-		requiresTwoMatrices: false,
-		requiresConstant: false,
-	},
-	{
-		id: "ref-rref",
-		title: "REF/RREF",
-		requiresTwoMatrices: false,
-		requiresConstant: false,
-	},
+  {
+    id: "add",
+    title: "Addition",
+    requiresTwoMatrices: true,
+    requiresConstant: false,
+  },
+  {
+    id: "subtract",
+    title: "Subtraction",
+    requiresTwoMatrices: true,
+    requiresConstant: false,
+  },
+  {
+    id: "multiplication",
+    title: "Multiplication",
+    requiresTwoMatrices: true,
+    requiresConstant: false,
+  },
+  {
+    id: "k-multiplication",
+    title: "K-Multiplication",
+    requiresTwoMatrices: false,
+    requiresConstant: true,
+  },
+  {
+    id: "transpose",
+    title: "Transpose",
+    requiresTwoMatrices: false,
+    requiresConstant: false,
+  },
+  {
+    id: "determinant",
+    title: "Determinant",
+    requiresTwoMatrices: false,
+    requiresConstant: false,
+  },
+  {
+    id: "cramers",
+    title: "Cramer's Rule",
+    requiresTwoMatrices: false,
+    requiresConstant: false,
+  },
+  {
+    id: "ref-rref",
+    title: "REF/RREF",
+    requiresTwoMatrices: false,
+    requiresConstant: false,
+  },
 ];
 
 export default function Hero() {
-	const [operation, setOperation] = useState(OPERATION_LIST[4]);
-	const [matrixSize, setMatrixSize] = useState({ rows: 2, columns: 2 });
-	const [matrix, setMatrix] = useState([
-		[0, 0],
-		[0, 0],
-	]);
-	const [secondMatrix, setSecondMatrix] = useState([
-		[0, 0],
-		[0, 0],
-	]);
-	const [result, setResult] = useState("");
+  const [operation, setOperation] = useState(OPERATION_LIST[4]);
+  const [matrixSize, setMatrixSize] = useState({ rows: 2, columns: 2 });
+  const [matrix, setMatrix] = useState([
+    [0, 0],
+    [0, 0],
+  ]);
+  const [secondMatrix, setSecondMatrix] = useState([
+    [0, 0],
+    [0, 0],
+  ]);
+  const [result, setResult] = useState("");
 
-	return (
-		<>
-			<main className="main">
-				<Navbar
-					operation={operation}
-					setOperation={(object) => setOperation(object)}
-				/>
+  return (
+    <>
+      <main className="main">
+        <Navbar
+          operation={operation}
+          setOperation={(object) => setOperation(object)}
+        />
 
-				<main className="main-wrapper">
-					<h2 className="operation-header">Matrix {operation.title}:</h2>
+        <main className="main-wrapper">
+          <h2 className="operation-header">Matrix {operation.title}:</h2>
 
-					<MatrixPrompt
-						matrixSize={matrixSize}
-						setMatrixSize={(object) => setMatrixSize(object)}
-						matrix={matrix}
-						setMatrix={(object) => setMatrix(object)}
-						secondMatrix={secondMatrix}
-						setSecondMatrix={(object) => setSecondMatrix(object)}
-						operation={operation}
-					></MatrixPrompt>
+          <MatrixPrompt
+            matrixSize={matrixSize}
+            setMatrixSize={(object) => setMatrixSize(object)}
+            matrix={matrix}
+            setMatrix={(object) => setMatrix(object)}
+            secondMatrix={secondMatrix}
+            setSecondMatrix={(object) => setSecondMatrix(object)}
+            operation={operation}
+          ></MatrixPrompt>
 
-					<MatrixResult
-						matrix={matrix}
-						result={result}
-						setResult={(object) => setResult(object)}
-					></MatrixResult>
-				</main>
-			</main>
-		</>
-	);
+          <MatrixResult
+            matrix={matrix}
+            result={result}
+            setResult={(object) => setResult(object)}
+          ></MatrixResult>
+        </main>
+      </main>
+    </>
+  );
 }
--- src/components/Hero/MatrixPrompt/MatrixInput/MatrixInput.jsx
+++ src/components/Hero/MatrixPrompt/MatrixInput/MatrixInput.jsx
@@ -2,46 +2,46 @@ import React from "react";
 import "./input.css";
 
 export default function MatrixInput({ matrixSize, setMatrix }) {
-	let matrix = Array(matrixSize.rows);
-	for (let r = 0; r < matrixSize.rows; r++) {
-		matrix[r] = new Array(matrixSize.columns).fill(0);
-	}
+  let matrix = Array(matrixSize.rows);
+  for (let r = 0; r < matrixSize.rows; r++) {
+    matrix[r] = new Array(matrixSize.columns).fill(0);
+  }
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		let count = 0;
-		for (let r = 0; r < matrixSize.rows; r++) {
-			for (let c = 0; c < matrixSize.columns; c++) {
-				// If the floating point number cannot be parsed, we set 0 for this value
-				matrix[r][c] = !isNaN(parseFloat(event.target[count].value))
-					? parseFloat(event.target[count].value)
-					: 0;
-				count += 1;
-			}
-		}
-		setMatrix(matrix);
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let count = 0;
+    for (let r = 0; r < matrixSize.rows; r++) {
+      for (let c = 0; c < matrixSize.columns; c++) {
+        // If the floating point number cannot be parsed, we set 0 for this value
+        matrix[r][c] = !isNaN(parseFloat(event.target[count].value))
+          ? parseFloat(event.target[count].value)
+          : 0;
+        count += 1;
+      }
+    }
+    setMatrix(matrix);
+  };
 
-	return (
-		<form className="form-input" onSubmit={handleSubmit}>
-			{matrix.map((row, indexRow = 1) => {
-				return (
-					<div key={indexRow}>
-						{row.map((item, indexColumn = 1) => {
-							return (
-								<input
-									className="num-input matrix-entry"
-									key={indexRow + " " + indexColumn}
-									type="text"
-									defaultValue={0}
-									name={indexRow + "," + indexColumn}
-								/>
-							);
-						})}
-					</div>
-				);
-			})}
-			<button className="input-submit">Calculate!</button>
-		</form>
-	);
+  return (
+    <form className="form-input" onSubmit={handleSubmit}>
+      {matrix.map((row, indexRow = 1) => {
+        return (
+          <div key={indexRow}>
+            {row.map((item, indexColumn = 1) => {
+              return (
+                <input
+                  className="num-input matrix-entry"
+                  key={indexRow + " " + indexColumn}
+                  type="text"
+                  defaultValue={0}
+                  name={indexRow + "," + indexColumn}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+      <button className="input-submit">Calculate!</button>
+    </form>
+  );
 }
