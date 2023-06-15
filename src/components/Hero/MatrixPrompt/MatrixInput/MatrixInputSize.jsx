0d4a7ea18afd2216e84eeaca20473e0aad8eb856
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-06-15 12:28:02 -0700

    Handled rows and columns input change in one single function

--- src/components/Hero/MatrixPrompt/MatrixInput/MatrixInputSize.jsx
+++ src/components/Hero/MatrixPrompt/MatrixInput/MatrixInputSize.jsx
@@ -1,42 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./input.css";
 
 export default function MatrixInputSize({ setMatrixSize }) {
+  const handleChange = (e) => {
+    const sizeInput = parseInt(e.target.value) ?? 0;
+    if (sizeInput > 0) {
+      e.target.name === "rows-input"
+        ? setMatrixSize((prevSize) => ({
+            ...prevSize,
+            rows: sizeInput,
+          }))
+        : setMatrixSize((prevSize) => ({
+            ...prevSize,
+            columns: sizeInput,
+          }));
+    }
+  };
+
   return (
     <form>
       <label>Matrix Size: </label>
       <input
+        name="rows-input"
         className="num-input"
         type="number"
         defaultValue={2}
         min={1}
-        onChange={(e) => {
-          //   const sizeInput = sizeInput != null ?? parseInt(e.target.value);
-          const sizeInput = parseInt(e.target.value);
-          if (sizeInput !== 0) {
-            setMatrixSize((prevSize) => ({
-              ...prevSize,
-              rows: sizeInput,
-            }));
-          }
-        }}
+        onChange={handleChange}
       />
       <span>X</span>
       <input
+        name="columns-input"
         className="num-input"
         type="number"
         defaultValue={2}
         min={1}
-        onChange={(e) => {
-          //   const sizeInput = sizeInput != null ?? parseInt(e.target.value);
-          const sizeInput = parseInt(e.target.value);
-          if (sizeInput > 0) {
-            setMatrixSize((prevSize) => ({
-              ...prevSize,
-              columns: sizeInput,
-            }));
-          }
-        }}
+        onChange={handleChange}
       />
     </form>
   );
