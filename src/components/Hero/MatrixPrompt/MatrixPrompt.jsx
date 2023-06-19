a2eed268cb40451dbe1927590a1fe03bd24afcc4
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-06-19 14:14:27 -0700

    Fixed React key issues with frament

--- src/components/Hero/MatrixPrompt/MatrixPrompt.jsx
+++ src/components/Hero/MatrixPrompt/MatrixPrompt.jsx
@@ -23,13 +23,19 @@ export default function MatrixPrompt({
     setConstant(event.target.value);
   };
 
-  const handleConstantMatrixChange = (event) => {};
+  const handleConstantMatrixChange = (event) => {
+    event.preventDefault();
+    let constantB = [];
+    // Destructure the array obtained from the name. For example, [0, 0] will be row = 0, col = 0.
+    const row = parseInt(event.target.name);
+    // Check whether the input entry was a number. If not, default to 0
+    constantB[row] = !isNaN(parseFloat(event.target.value))
+      ? parseFloat(event.target.value)
+      : 0;
+    setMatrix(constantB);
+  };
 
   const handleSubmit = () => {
-    // console.log(matrix);
-    // console.log(secondMatrix);
-    // console.log(constant);
-    // console.log(operation);
     if (operation.requiresTwoMatrices) {
       setResult(operation.computeCalculation(matrix, secondMatrix));
     } else if (operation.requiresConstant) {
@@ -37,14 +43,18 @@ export default function MatrixPrompt({
     } else {
       setResult(operation.computeCalculation(matrix));
     }
+    if (operation.requiresConstantMatrix) {
+      setResult(operation.computeCalculation(matrix, constantMatrix));
+    }
   };
 
   useEffect(() => {
     let constantB = [];
     for (let r = 0; r < matrixSize.rows; r++) {
-      constantB[r] = constantMatrix[r] ? constantMatrix[r] : 0;
+      constantB[r] = constantMatrix[r] !== undefined ? constantMatrix[r] : 0;
     }
     setConstantMatrix(constantB);
+    console.log(constantMatrix);
   }, [matrixSize]);
 
   return (
@@ -89,22 +99,24 @@ export default function MatrixPrompt({
         )}
         {operation.requiresConstantMatrix && (
           <div className="wrapper-column">
-            <form>
-              <label>Constant Matrix B:</label>
+            <form className="constant-matrix-form">
+              <label className="constant-matrix-label">
+                Constant Matrix B:
+              </label>
               <br />
               {constantMatrix.map((row, indexRow = 1) => {
                 return (
-                  <>
+                  <React.Fragment key={indexRow + "fragment"}>
                     <input
                       className="num-input matrix-entry"
-                      key={indexRow + "   "}
+                      key={indexRow}
                       type="text"
                       defaultValue={0}
                       name={indexRow}
                       onChange={handleConstantMatrixChange}
                     />
-                    <br />
-                  </>
+                    <br key={indexRow + "break"} />
+                  </React.Fragment>
                 );
               })}
             </form>
