a61f8152c94d06cbd06175044893e83548907052
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-06-26 10:37:21 -0700

    Started working on responsiveness

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -18,8 +18,8 @@ export default function Help({ isOpen, setIsOpen, toggleSidebar }) {
         <p className="help-contents">
           MatriXpert is a free, open source Matrix calculator built in ReactJS.
           It provides well layed-out solutions to many of the concepts that
-          students struggle with when then encounter Linear Algebra for the
-          first time.
+          students struggle with when encountering Linear Algebra for the first
+          time.
         </p>
         <h3 className="sub-header smaller-header">How to Use</h3>
         <p className="help-contents">
@@ -28,9 +28,16 @@ export default function Help({ isOpen, setIsOpen, toggleSidebar }) {
         </p>
         <p className="help-contents">
           Then input your matrix by filling in the forms (invalid inputs will
-          automatically be evaluated to 0). Finally, press the Calculate button
-          to display the result of your chosen operation, along with some
-          further explanations as to how the result is calculated.
+          automatically be evaluated to 0). Finally, press the "Calculate"
+          button to display the result of your chosen operation, along with some
+          further explanations as to how the result was calculated.
+        </p>
+        <p className="help-contents">
+          Experienced some bugs? You can{" "}
+          <a href="https://github.com/tomasohCHOM/MatriXpert/issues">
+            come here
+          </a>{" "}
+          to report them. Thank you for using MatriXpert!
         </p>
       </nav>
       <div
